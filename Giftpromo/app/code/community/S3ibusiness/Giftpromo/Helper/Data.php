b261d4f2c2febd6c5d7db9ac2cd15e61f4024116
Ahmed mahi <1hmedmahi@gmail.com>
2020-04-22 03:50:00 +0000

    juste pour test l'IDE en ligne gitpod

--- Giftpromo/app/code/community/S3ibusiness/Giftpromo/Helper/Data.php
+++ Giftpromo/app/code/community/S3ibusiness/Giftpromo/Helper/Data.php
@@ -2,7 +2,7 @@
 /**
  * @category    S3ibusiness
  * @package     S3ibusiness_Giftpromo
- * @copyright   Copyright (c) 2011 S3i Business sarl au. (http://www.s3ibusiness.com)
+ * @copyright   Copyright (c) 2011 S3i Business sarl au. (http://www.s3ibusiness.com) jsute un test
  * @author      Ahmed Mahi <1hmedmahi@gmail.com>
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
